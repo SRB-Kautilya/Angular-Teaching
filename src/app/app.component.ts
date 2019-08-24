23e2176f22a34f8014177d042e0bf248ad056620
SRB-Kautilya <shashankreddy444@gmail.com>
2019-08-24 17:07:15 -0400

    "Template -driven Angular form"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  @ViewChild('f', { static: true }) signUpForm: NgForm;
+  defaultValue = "teacher";
+  answer = "";
+  genders = ["Male", "Female"]
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   gender: "",
+    //   questionAnswer: "",
+    //   secret: "teacher",
+    //   userData: { userame: "SugestedName", email: "" }
+    // })
+    this.signUpForm.form.patchValue({
+
+      userData: { userame: "SugestedName" }
+    })
+  }
+
+  // onSubmit(form:NgForm) {
+  //   console.log('FORM:',form)
+  // }
+
+  //Accessing form via Viewchild
+
+  onSubmit() {
+    console.log(this.signUpForm)
   }
 }
